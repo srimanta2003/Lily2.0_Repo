5466f24711c67054d20b827cb279bb58f692891d
Venom-TEST-BOTS <162750365+Venom-TEST-BOTS@users.noreply.github.com>
2024-03-09 02:48:25 +0530

    Create __main__.py

---
+++ LilyXMusic/__main__.py
@@ -0,0 +1,62 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from GroovyMusic import LOGGER, app, userbot
+from GroovyMusic.core.call import Anony
+from GroovyMusic.misc import sudo
+from GroovyMusic.plugins import ALL_MODULES
+from GroovyMusic.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("GroovyMusic.plugins" + all_module)
+    LOGGER("GroovyMusic.plugins").info("Successfully Imported Modules...")
+    await userbot.start()
+    await Anony.start()
+    try:
+        await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("GroovyMusic").error(
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+        )
+        exit()
+    except:
+        pass
+    await Anony.decorators()
+    LOGGER("GroovyMusic").info(
+        "𝙼𝙴𝚁𝙸 𝙹𝙰𝙰𝙽 𝙼𝙰𝚉𝙴 𝙺𝙰𝚁𝙾 𝙰𝙱 𝙹𝙰𝙺𝙴 𝙰𝙶𝙰𝚁 𝙺𝙾𝙸 𝙸𝚂𝚂𝚄𝙴 𝙷𝚄𝙰 𝚃𝙾𝙷 @its_Aryaan 𝚈𝙰𝙷𝙰 𝙳𝙼 𝙺𝙰𝚁 𝙻𝙴𝙽𝙰"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("GroovyMusic").info("Stopping 𝙻𝚒𝚕𝚢𝚇𝙼𝚞𝚜𝚒𝚌 Music Bot...")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
