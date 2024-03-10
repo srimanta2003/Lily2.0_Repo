87e58f2ab2a064e11b004840cfe129644180b9dc
Venom-TEST-BOTS <162750365+Venom-TEST-BOTS@users.noreply.github.com>
2024-03-11 03:56:14 +0530

    Update start.py

--- LilyXMusic/utils/inline/start.py
+++ LilyXMusic/utils/inline/start.py
@@ -1,16 +1,16 @@
 from pyrogram.types import InlineKeyboardButton
 
 import config
-from GroovyMusic import app
+from LilyXMusic import app
 
 
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["LY_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["LY_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
@@ -20,17 +20,17 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
+                text=_["LY_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [InlineKeyboardButton(text=_["LY_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["LY_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["LY_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["LY_B_6"], url=config.SUPPORT_CHANNEL),
             
         ],
     ]
