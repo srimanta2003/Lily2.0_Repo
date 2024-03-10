040c116015539710021a8e4c8d95413bd84e378d
Venom-TEST-BOTS <162750365+Venom-TEST-BOTS@users.noreply.github.com>
2024-03-11 04:36:49 +0530

    Update queue.py

--- LilyXMusic/plugins/tools/queue.py
+++ LilyXMusic/plugins/tools/queue.py
@@ -6,12 +6,12 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 
 import config
-from GroovyMusic import app
-from GroovyMusic.misc import db
-from GroovyMusic.utils import AnonyBin, get_channeplayCB, seconds_to_min
-from GroovyMusic.utils.database import get_cmode, is_active_chat, is_music_playing
-from GroovyMusic.utils.decorators.language import language, languageCB
-from GroovyMusic.utils.inline import queue_back_markup, queue_markup
+from LilyXMusic import app
+from LilyXMusic.misc import db
+from LilyXMusic.utils import AnonyBin, get_channeplayCB, seconds_to_min
+from LilyXMusic.utils.database import get_cmode, is_active_chat, is_music_playing
+from LilyXMusic.utils.decorators.language import language, languageCB
+from LilyXMusic.utils.inline import queue_back_markup, queue_markup
 from config import BANNED_USERS
 
 basic = {}
