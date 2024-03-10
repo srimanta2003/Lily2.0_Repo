8ee6e373ff934252fc89c960b4b1aab07947f820
Venom-TEST-BOTS <162750365+Venom-TEST-BOTS@users.noreply.github.com>
2024-03-11 04:08:17 +0530

    Update help.py

--- LilyXMusic/utils/inline/help.py
+++ LilyXMusic/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from GroovyMusic import app
+from LilyXMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
