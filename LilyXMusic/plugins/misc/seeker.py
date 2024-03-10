dd211dead1935267516207a8ec3fe2347cec6f27
Venom-TEST-BOTS <162750365+Venom-TEST-BOTS@users.noreply.github.com>
2024-03-11 04:23:30 +0530

    Update seeker.py

--- LilyXMusic/plugins/misc/seeker.py
+++ LilyXMusic/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from GroovyMusic.misc import db
-from GroovyMusic.utils.database import get_active_chats, is_music_playing
+from LilyXMusic.misc import db
+from LilyXMusic.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
