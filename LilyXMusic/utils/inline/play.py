5a5fd7b0acc08e1f8641cf3755bf91185d444549
Venom-TEST-BOTS <162750365+Venom-TEST-BOTS@users.noreply.github.com>
2024-03-11 04:07:54 +0530

    Update play.py

--- LilyXMusic/utils/inline/play.py
+++ LilyXMusic/utils/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from GroovyMusic.utils.formatters import time_to_seconds
+from LilyXMusic.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
