028a82826edd474b0429131d564050392cb4cd71
Venom-TEST-BOTS <162750365+Venom-TEST-BOTS@users.noreply.github.com>
2024-03-11 04:23:04 +0530

    Update watcher.py

--- LilyXMusic/plugins/misc/watcher.py
+++ LilyXMusic/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from GroovyMusic import app
-from GroovyMusic.core.call import Anony
+from LilyXMusic import app
+from LilyXMusic.core.call import Anony
 
 welcome = 20
 close = 30
