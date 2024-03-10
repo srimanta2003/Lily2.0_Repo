5f608c22e8f0df0793ad487930a0d2199b5bc01d
Venom-TEST-BOTS <162750365+Venom-TEST-BOTS@users.noreply.github.com>
2024-03-11 03:52:42 +0530

    Update stream.py

--- LilyXMusic/utils/stream/stream.py
+++ LilyXMusic/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from GroovyMusic import Carbon, YouTube, app
-from GroovyMusic.core.call import Anony
-from GroovyMusic.misc import db
-from GroovyMusic.utils.database import add_active_video_chat, is_active_chat
-from GroovyMusic.utils.exceptions import AssistantErr
-from GroovyMusic.utils.inline import aq_markup, close_markup, stream_markup
-from GroovyMusic.utils.pastebin import AnonyBin
-from GroovyMusic.utils.stream.queue import put_queue, put_queue_index
-from GroovyMusic.utils.thumbnails import get_thumb
+from LilyXMusic import Carbon, YouTube, app
+from LilyXMusic.core.call import Anony
+from LilyXMusic.misc import db
+from LilyXMusic.utils.database import add_active_video_chat, is_active_chat
+from LilyXMusic.utils.exceptions import AssistantErr
+from LilyXMusic.utils.inline import aq_markup, close_markup, stream_markup
+from LilyXMusic.utils.pastebin import AnonyBin
+from LilyXMusic.utils.stream.queue import put_queue, put_queue_index
+from LilyXMusic.utils.thumbnails import get_thumb
 
 
 async def stream(
