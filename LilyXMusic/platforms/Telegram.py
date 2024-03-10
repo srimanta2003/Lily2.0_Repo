8d90e131d026e385e25a4439402a5ed601b11292
Venom-TEST-BOTS <162750365+Venom-TEST-BOTS@users.noreply.github.com>
2024-03-11 04:45:40 +0530

    Update Telegram.py

--- LilyXMusic/platforms/Telegram.py
+++ LilyXMusic/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from GroovyMusic import app
-from GroovyMusic.utils.formatters import (
+from LilyXMusic import app
+from LilyXMusic.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
