1a7c8bb7f1dce930e036991f2dfe39f347f66305
Venom-TEST-BOTS <162750365+Venom-TEST-BOTS@users.noreply.github.com>
2024-03-12 23:27:01 +0530

    Update ping.py

--- LilyXMusic/plugins/tools/ping.py
+++ LilyXMusic/plugins/tools/ping.py
@@ -11,7 +11,7 @@ from LilyXMusic.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
 
 
-@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["ping", "zinda"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
