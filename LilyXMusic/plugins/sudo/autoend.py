73c64943f182ffc36e0af49bf4e851c45aff73a2
Venom-TEST-BOTS <162750365+Venom-TEST-BOTS@users.noreply.github.com>
2024-03-12 23:46:20 +0530

    Update autoend.py

--- LilyXMusic/plugins/sudo/autoend.py
+++ LilyXMusic/plugins/sudo/autoend.py
@@ -8,17 +8,17 @@ from LilyXMusic.utils.database import autoend_off, autoend_on
 
 @app.on_message(filters.command("autoend") & SUDOERS)
 async def auto_end_stream(_, message: Message):
-    usage = "<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
+    usage = "<b>𝐄xᴀᴍᴘʟᴇ :</b>\n\n/autoend [𝐄ɴᴀʙʟᴇ | 𝐃ɪsᴀʙʟᴇ]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip().lower()
     if state == "enable":
         await autoend_on()
         await message.reply_text(
-            "» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
+            "» 𝐀ᴜᴛᴏ 𝐄ɴᴅ 𝐒ᴛʀᴇᴀᴍ 𝐄ɴᴀʙʟᴇᴅ.\n\n𝐀ssɪsᴛᴀɴᴛ 𝐖ɪʟʟ 𝐀ᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ 𝐋ᴇᴀᴠᴇ 𝐓ʜᴇ 𝐕ɪᴅᴇᴏᴄʜᴀᴛ 𝐀ғᴛᴇʀ 𝐅ᴇᴡ 𝐌ɪɴs 𝐖ʜᴇɴ 𝐍ᴏ 𝐎ɴᴇ 𝐈s 𝐋ɪsᴛᴇɴɪɴɢ."
         )
     elif state == "disable":
         await autoend_off()
-        await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
+        await message.reply_text("» 𝐀ᴜᴛᴏ 𝐄ɴᴅ 𝐒ᴛʀᴇᴀᴍ 𝐃ɪsᴀʙʟᴇᴅ.")
     else:
         await message.reply_text(usage)
