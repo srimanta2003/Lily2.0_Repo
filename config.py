304400d16f7d234898cbacda6375ee2f279d9878
Venom-TEST-BOTS <162750365+Venom-TEST-BOTS@users.noreply.github.com>
2024-03-15 13:52:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/thecchub")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+eOndzGjht5FhNGY1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+af1GQdKbpEU2Y2Fl")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False)) 
